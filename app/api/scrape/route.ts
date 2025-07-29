64b2f20661226790ebedef7dc2635e2f65793f09
Augustine Putman <putmanaugustine@gmail.com>
2025-07-29 09:14:52 -0400

    Laid out groundwork of code for handling database errors when adding automatically scraped deals

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -1,6 +1,8 @@
 import { auth } from "@/auth";
+import axios from "axios";
 import { createClient } from "redis";
 import { NextRequest, NextResponse } from "next/server";
+import {v4 as uuidv4} from 'uuid';
 
 const redisClient = createClient({
   url: process.env.REDIS_CLIENT_URL,
@@ -19,18 +21,24 @@ export async function POST(request: NextRequest) {
     );
   }
 
+  // connect to a websocket, this will remain open until the request is finished
+  const receivingWebsocket = new WebSocket("ws://localhost:8080");
+
+  // send data to Redis, so it can be parsed
   const formData = await request.formData();
   const url = formData.get("url");
   const firmName = formData.get("firmName");
   const userId = formData.get("userId");
 
+  receivingWebsocket.onopen = () => {
+    console.log("Connected to websocket");
+    receivingWebsocket.send(JSON.stringify({type: "register", userId: userId}));
+  }
+
   try {
     await redisClient.lPush(
       "submissions",
       JSON.stringify({ url, userId, firmName })
-    );
-    return NextResponse.json(
-      { message: "Submission received" }
     );
   } catch (error) {
     console.error("Error pushing to Redis:", error);
@@ -38,5 +46,12 @@ export async function POST(request: NextRequest) {
       { error: "Error pushing to Redis"},
       {status: 500}
     );
+  }
+
+  receivingWebsocket.onmessage = (event) => {
+    const result = JSON.parse(event.data);
+    console.log("Result", result);
+    // upload to database
+    // if database fails, store on websocket for 5 minutes, then try again. If it fails 3 times in a row, wait 1 hour. If that fails 3 times, wait 1 day
   }
 }
